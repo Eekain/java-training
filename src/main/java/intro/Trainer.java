9c3226ccb9ada83ff277f9727addce043ef92fb2
Eekain <bkg.eekain@gmail.com>
2022-05-30 15:28:24 +0200

    added getters and setters

--- src/main/java/intro/Trainer.java
+++ src/main/java/intro/Trainer.java
@@ -4,6 +4,22 @@ public class Trainer {
 
     String name;
     int yearOfBirth;
+    public int getAge(int currentYear){
+        return currentYear - yearOfBirth;
+    }
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
 }
--- src/main/java/intro/TrainerMain.java
+++ src/main/java/intro/TrainerMain.java
@@ -6,16 +6,16 @@ public class TrainerMain {
         john.name = "Johhny Do";
         john.yearOfBirth = 1980;
 
-        System.out.println(john.name);
-        System.out.println(john.yearOfBirth);
-
+        System.out.println(john.getName());
+        System.out.println(john.getYearOfBirth());
+        System.out.println(john.getAge(2022));
         Trainer jack = new Trainer();
         jack.name = "Jack Doehs2";
         jack.yearOfBirth = 1990;
-        System.out.printf("%s year of birth is: %d\n", jack.name, jack.yearOfBirth);
+        System.out.printf("\u001B[31m%s year of birth is: %d, and age: %d\n\u001B[0m", jack.getName(), jack.getYearOfBirth(), jack.getAge(2022));
 
         Trainer rando = new Trainer();
-        System.out.println("\n" + rando.name + "\n" + rando.yearOfBirth);
+        System.out.println("\n" + "\u001B[32m" + rando.getName() + "\n" + rando.getYearOfBirth() + "\u001B[0m");
 
         System.out.println(rando);
 
