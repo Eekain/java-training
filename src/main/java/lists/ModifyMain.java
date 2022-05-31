14074e47cb5be9e24034d556d2924af65ad235b0
Eekain <bkg.eekain@gmail.com>
2022-05-31 16:36:16 +0200

    var és iterátor

---
+++ src/main/java/lists/ModifyMain.java
@@ -0,0 +1,18 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+// \java-bgn-2022-05-30 a training 360 githubján
+public class ModifyMain {
+    public static void main(String[] args) {
+        List<Integer> numbers= new ArrayList<>(List.of(1,3,4));
+        var it = numbers.iterator(); //this is an iterator
+        while (it.hasNext()){
+            var number = it.next();
+            if (number % 2 == 0){
+                it.remove();
+            }
+        }
+    }
+}
