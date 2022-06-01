98f344901b0c1bf0b7eaeba5788b11e044a2e1a7
Eekain <bkg.eekain@gmail.com>
2022-06-01 13:55:09 +0200

    it is wednesday my dudes _ add_All kivéve

--- src/test/java/office/OfficeTest.java
+++ src/test/java/office/OfficeTest.java
@@ -24,37 +24,37 @@ class OfficeTest {
 
     @Test
     void getNames() {
-        List<String> names = new ArrayList<>();
-        names.addAll(List.of("Tárgyaló", "Aula", "Kistárgyaló", "Előadó", "Vizsgaterem"));
+        List<String> names = new ArrayList<>(List.of("Tárgyaló", "Aula", "Kistárgyaló", "Előadó", "Vizsgaterem"));
+
         assertLinesMatch(names, office.getNames());
     }
 
     @Test
     void getNamesReverse() {
-        List<String> names = new ArrayList<>();
-        names.addAll(List.of("Vizsgaterem", "Előadó", "Kistárgyaló", "Aula", "Tárgyaló"));
+        List<String> names = new ArrayList<>(List.of("Vizsgaterem", "Előadó", "Kistárgyaló", "Aula", "Tárgyaló"));
+
         assertLinesMatch(names, office.getNamesReverse());
     }
 
     @Test
     void getEvenNames() {
-        List<String> names = new ArrayList<>();
-        names.addAll(List.of("Tárgyaló", "Kistárgyaló", "Vizsgaterem"));
+        List<String> names = new ArrayList<>(List.of("Tárgyaló", "Kistárgyaló", "Vizsgaterem"));
+
         assertLinesMatch(names, office.getEvenNames());
     }
 
     @Test
     void getAreas() {
-        List<Integer> areas = new ArrayList<>();
-        areas.addAll(List.of(15*20, 20*20, 3*5, 15*32, 10*20));
+        List<Integer> areas = new ArrayList<>(List.of(15*20, 20*20, 3*5, 15*32, 10*20));
+
         for(int i = 0; i< areas.size(); i++){
             assertEquals(office.getAreas().get(i), areas.get(i));
         }
     }
     @Test
     void getAreas_wrong() {
-        List<Integer> areas = new ArrayList<>();
-        areas.addAll(List.of(15*20+1, 20*20-2, 3*5-3, 15*32*0, 10*20-2));
+        List<Integer> areas = new ArrayList<>(List.of(15*20+1, 20*20-2, 3*5-3, 15*32*0, 10*20-2));
+
         for(int i = 0; i< areas.size(); i++){
             assertNotEquals(office.getAreas().get(i), areas.get(i));
         }
